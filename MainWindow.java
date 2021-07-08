72e2f8011cd4681ed10aab5a3f79752968ac3925
zainab946 <zainab.zahid2002@gmail.com>
2021-07-08 01:19:02 -0700

    done with bash

---
+++ MainWindow.java
@@ -0,0 +1,28 @@
+package application;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainWindow extends Application{
+	 
+	public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Calculator");
+//        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+        ((SampleController)loader.getController()).init(stage);
+        stage.show();
+    }
+
+    public void run() {
+        launch();
+    }
+}
